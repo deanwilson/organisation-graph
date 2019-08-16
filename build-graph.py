73e756b53fce6c06a0735800f88fc29a3be930a8
Dean Wilson <dean.wilson@gmail.com>
2019-08-16 16:52:44 +0100

    Update the code to pass flake8
    And reformat with black for consistency

--- build-graph.py
+++ build-graph.py
@@ -11,7 +11,7 @@ from yaml import load, SafeLoader
 def _inflate_yaml(file_name):
     yaml = None
 
-    with open(file_name, 'r') as yaml_file:
+    with open(file_name, "r") as yaml_file:
         yaml = yaml_file.read()
 
     inflated_yaml = load(yaml, Loader=SafeLoader)
@@ -52,9 +52,9 @@ def main(args):
     # inconsistent
     graph.delete_all()
 
-    ## Extract the data
+    # Extract the data
     people = set()
-    # make these comprehensions?
+    # TODO make these comprehensions?
     roles = set()
     departments = set()
     teams = set()
@@ -64,22 +64,22 @@ def main(args):
 
         person = staff[name]
 
-        roles.add(person['is_a'])
+        roles.add(person["is_a"])
 
-        if 'manages' in person:
-            reports = person['manages']
+        if "manages" in person:
+            reports = person["manages"]
 
             for report in reports:
                 people.add(report)
 
-        if 'member_of' in person:
-            departments.add(person['member_of'])
+        if "member_of" in person:
+            departments.add(person["member_of"])
 
-        if 'assigned_to' in person:
-            for team in person['assigned_to']:
+        if "assigned_to" in person:
+            for team in person["assigned_to"]:
                 teams.add(team)
 
-    ## Add the base nodes
+    # Add the base nodes
 
     for person in people:
         node = Node("Person", name=person)
@@ -97,8 +97,8 @@ def main(args):
         node = Node("Department", name=department)
         graph.create(node)
 
-    ## Build the relationships
-    ### People
+    # Build the relationships
+    # People
     for employee_name in staff:
         employee = staff[employee_name]
 
@@ -106,56 +106,57 @@ def main(args):
         # employee_name is a string, person_node is a graph object
         person_node = matcher.match("Person", name=employee_name).first()
 
-        ## Management relationships
-        manages = employee.get('manages', [])
+        # Management relationships
+        manages = employee.get("manages", [])
         for report in manages:
             # inefficient to do this lookup everytime? Cache on node creation?
             report_node = matcher.match("Person", name=report).first()
             manage_rel = Relationship(person_node, "manages", report_node)
             graph.create(manage_rel)
 
-        ## Role relationships
-        role = employee['is_a']
+        # Role relationships
+        role = employee["is_a"]
         role_node = matcher.match("Role", name=role).first()
         role_rel = Relationship(person_node, "is a", role_node)
         graph.create(role_rel)
 
-        ## Team relationships
-        teams = employee.get('assigned_to', [])
+        # Team relationships
+        teams = employee.get("assigned_to", [])
         for team in teams:
             team_node = matcher.match("Team", name=team).first()
             team_rel = Relationship(person_node, "assigned to", team_node)
             graph.create(team_rel)
 
-        ## Tech leads
-        leads = employee.get('tech_lead', [])
+        # Tech leads
+        leads = employee.get("tech_lead", [])
         for team in leads:
             team_node = matcher.match("Team", name=team).first()
             lead_rel = Relationship(person_node, "tech lead", team_node)
             graph.create(lead_rel)
 
-        ## Departments / member_of
-        department = employee['member_of']
+        # Departments / member_of
+        department = employee["member_of"]
         department_node = matcher.match("Department", name=department).first()
         department_rel = Relationship(person_node, "member of", department_node)
         graph.create(department_rel)
 
-
-    ## Services
+    # Services
     for service_name in services["services"]:
         service = services["services"][service_name]
 
         service_node = Node("Service", name=service_name)
-        graph.create(service_node) # TODO: might be redundent due to graph.create(owner_rel)
+        graph.create(
+            service_node
+        )  # TODO: might be redundent due to graph.create(owner_rel)
 
         matcher = NodeMatcher(graph)
 
-        owner = service['owner']
+        owner = service["owner"]
         team_node = matcher.match("Team", name=owner).first()
         owner_rel = Relationship(team_node, "owns", service_node)
         graph.create(owner_rel)
 
-        for tech in service['technology']:
+        for tech in service["technology"]:
 
             # if we got the node, use it
             tech_node = matcher.match("Technology", name=tech).first()
@@ -169,23 +170,34 @@ def main(args):
             graph.create(tech_rel)
 
 
-## TODO
-### Abstract out the addition of the node and relationship and call it once in
-### the main loop? "
+# TODO
+# Abstract out the addition of the node and relationship and call it once in
+# the main loop? "
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Build the organisations graph')
+    parser = argparse.ArgumentParser(description="Build the organisations graph")
 
-    parser.add_argument('--data', type=str, default='data',
-                        help='A directory containing your yaml data files')
-    parser.add_argument('--services', type=str, default='services.yaml',
-                        help='A valid yaml file containing your service information')
-    parser.add_argument('--staff', type=str, default='staff.yaml',
-                        help='A valid yaml file containing your staff information')
+    parser.add_argument(
+        "--data",
+        type=str,
+        default="data",
+        help="A directory containing your yaml data files",
+    )
+    parser.add_argument(
+        "--services",
+        type=str,
+        default="services.yaml",
+        help="A valid yaml file containing your service information",
+    )
+    parser.add_argument(
+        "--staff",
+        type=str,
+        default="staff.yaml",
+        help="A valid yaml file containing your staff information",
+    )
 
     args = parser.parse_args()
-
 
     main(args)
