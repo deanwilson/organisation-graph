0ea977c886366ae7d20beadab43708cd37b0f617
Dean Wilson <dean.wilson@gmail.com>
2019-08-16 17:11:11 +0100

    Extract the Employee class
    Seperating the Employee class allows it to be reused in other places,
    such as the main graph building script.

---
+++ orggraph/employee.py
@@ -0,0 +1,17 @@
+class Employee:
+    def __init__(self, name, data):
+        self.name = name
+
+        self.role = data["is_a"]
+        self.member_of = data["member_of"]
+
+        self.manager = None
+
+        self.assigned_to = data.get("assigned_to", [])
+        self.manages = data.get("manages", [])
+
+    def set_manager(self, manager):
+        self.manager = manager
+
+    def get_manager(self):
+        return self.manager
