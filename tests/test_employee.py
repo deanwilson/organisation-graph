8bafbe94a5c288abbf5ce6670964652eae071177
Dean Wilson <dean.wilson@gmail.com>
2019-08-17 08:09:51 +0100

    Add an employee test
    Ensure the object creation is working correctly as a safety net

---
+++ tests/test_employee.py
@@ -0,0 +1,23 @@
+"""Test cases for the Empployee object."""
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from orggraph.employee import Employee  # noqa: E402
+
+
+def test_creation():
+    """Test basic employee object creation."""
+    name = "Craig Hollis"
+    details = {
+        "aliases": ["Mr Immortal"],
+        "is_a": "Leader",
+        "manages": ["DeMarr Davis", "Dinah Soar"],
+        "member_of": "Great Lakes Avengers",
+        "assigned_to": "Mutant",
+    }
+
+    employee = Employee(name, details)
+
+    assert employee.aliases[0] == "Mr Immortal"
