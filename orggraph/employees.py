e516b273491a8aec15b96a6c385074aaec205c3e
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2019-08-21 17:46:30 +0100

    Add an Employee collection object
    This allows grouping of the employees and will replace the manual
    datastructure approach currently used to build the graph

---
+++ orggraph/employees.py
@@ -0,0 +1,22 @@
+from orggraph.dataloader import yaml_loader, file_contents
+from orggraph.employee import Employee
+
+
+class Employees:
+    def __init__(self, data_path):
+        self.path = data_path
+        self.employee_list = self._load_employees()
+
+    def _load_employees(self):
+        """Load the supplied employee YAML and convert it into Employee objects."""
+        employees = {}
+
+        employees = yaml_loader(file_contents(self.path))
+
+        for employee in employees:
+            employees[employee] = Employee(employee, employees[employee])
+
+        return employees
+
+    def employees(self):
+        return list(self.employee_list.values())
